a7f8bb5629a30e270a5dcfffb1a98ff5e82b390c
Azay18 <muhammadrizqiaz@gmail.com>
2018-12-11 08:52:56 +0700

    Initial commit

---
+++ app/models/User_model.php
@@ -0,0 +1,9 @@
+<?php
+
+class User_model {
+  private $nama = 'Muhammad Rizqi Az Zayyad';
+
+  public function getUser(){
+    return $this->nama;
+  }
+}
---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+  <h1 class="mt-4">About</h1>
+  <img src="<?= BASEURL; ?>/img/photo.jpg" alt="Rizqi" width="200" class="rounded-circle shadow">
+  <p>Nama saya <?= $data['nama'] ?> sebagai <?= $data['pekerjaan'] ?></p>
+</div>
